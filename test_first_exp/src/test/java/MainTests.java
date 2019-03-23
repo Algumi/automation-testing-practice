56ad3965d4b0991dda352507fde77d343abdbdcb
Alex <alex_jel@list.ru>
2019-03-23 16:42:37 +0400

    Finished second test case. Made some refactoring

--- test_first_exp/src/test/java/MainTests.java
+++ test_first_exp/src/test/java/MainTests.java
@@ -1,10 +1,10 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,6 +18,7 @@ import java.util.concurrent.TimeUnit;
 
 public class MainTests {
     private WebDriver driver;
+    private WebDriverWait waitTest;
 
     @BeforeClass
     public static void setupClass() {
@@ -29,21 +30,21 @@ public class MainTests {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://beru.ru/");
+        waitTest = new WebDriverWait(driver, 10);
+        //driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
     }
 
     @Test
     public void signInTest() {
         // closing pop-up window
         driver.findElement(By.className("_1ZYDKa22GJ")).click();
-        // opens the sign in page
-        driver.findElement(By.className("header2-nav__user")).click();
 
         // preforms steps to sign in
         SignInSteps.signIn(driver);
 
         // Test of changing button text from "Войти в аккаунт" to "Мой профиль"
-        WebElement profileName = (new WebDriverWait(driver, 10))
-                .until(ExpectedConditions.presenceOfElementLocated(By.className("header2__nav")));
+        WebElement profileName = waitTest.until
+                (ExpectedConditions.presenceOfElementLocated(By.className("header2__nav")));
         Assert.assertEquals(profileName.getText(),"Мой профиль",
                 "Button \"Войти в аккаунт\" was not changed to \"Мой профиль\".");
 
@@ -52,8 +53,8 @@ public class MainTests {
         Actions actor = new Actions(driver);
         actor.moveToElement(profileName).build().perform();
         // User name display test
-        WebElement userName = (new WebDriverWait(driver, 2))
-                .until(ExpectedConditions.presenceOfElementLocated(By.className("header2-user-menu__user-name")));
+        WebElement userName = waitTest.until
+                (ExpectedConditions.presenceOfElementLocated(By.className("header2-user-menu__user-name")));
         Assert.assertEquals(userName.getText(), "Тест Авто",
                 "Button \"Войти в аккаунт\" was not changed to \"Мой профиль\"." + userName.getText());
     }
@@ -62,34 +63,56 @@ public class MainTests {
 
     @Test
     public void cityChangeTest() {
-        WebDriverWait waitTest = new WebDriverWait(driver, 10);
         // closing pop-up window
         driver.findElement(By.className("_1ZYDKa22GJ")).click();
 
-        // element with city name
-        WebElement cityLine = driver.findElement(By.xpath
-                ("/html/body/div[1]/div/div[2]/div[2]/noindex/div/div[1]/div/div/div[1]/span/span[2]/span/span"));
-        String initialCity = cityLine.getText();
+        // element with city name on the top of the main page
+        WebElement cityLine = driver.findElement(By.className("region-form-opener"))
+                .findElement(By.className("link__inner"));
         cityLine.click();
 
-
+        // city input popup
+        WebElement cityPopup = waitTest.until
+                (ExpectedConditions.presenceOfElementLocated(By.className("region-select-form")));
         // city input field
-        WebElement loginInput = waitTest.until
-                (ExpectedConditions.presenceOfElementLocated(By.className("input__control")));
-        // entering new city name
-        loginInput.sendKeys(newCityName);
-        waitTest.until(ExpectedConditions.textToBe(By.className("input__control"), newCityName));
-        loginInput.submit();
+        WebElement cityField = cityPopup.findElement(By.className("input__control"));
+        cityField.sendKeys(newCityName);
+        // wait until entered text is displayed on popup
+        waitTest.until(ExpectedConditions.textToBePresentInElement(cityPopup, newCityName));
+        // submit changes
+        cityField.sendKeys(Keys.ENTER);
+        cityPopup.submit();
 
-        // click on continue with new city button
-        //WebElement changeCity = (new WebDriverWait(driver, 10)).until(ExpectedConditions.
-        //        presenceOfElementLocated(By.xpath("/html/body/div[7]/div/div/div[1]/div[1]/form/div/button")));
+        // wait until page loading is complete (footer social media elements are always loaded last)
+        waitTest.until(ExpectedConditions.visibilityOfElementLocated(By.className("footer__social-media")));
+        // city name line on the top of the main page
+        cityLine = driver.findElement(By.className("region-form-opener")).findElement(By.className("link__inner"));
+        // test of city name changes
+        Assert.assertEquals(newCityName, cityLine.getText(),
+                "City name on main page wasn't changed to" + newCityName);
 
         // preforms steps to sign in
-        //SignInSteps.signIn(driver);
+        SignInSteps.signIn(driver);
+
+        // goes to account settings
+        Actions actor = new Actions(driver);
+        WebElement myProfile = waitTest.until
+                (ExpectedConditions.presenceOfElementLocated(By.className("header2__nav")));
+        actor.moveToElement(myProfile).build().perform();
+        waitTest.until(ExpectedConditions.presenceOfElementLocated(By.className("header2-user-menu")));
+        driver.findElement(By.xpath("/html/body/div[7]/div[2]/div/ul[3]/li[3]/a")).click();
+        // city name on settings page
+        WebElement myCity = waitTest.until(ExpectedConditions.presenceOfElementLocated
+                (By.className("settings-list__title"))).findElement(By.className("link__inner"));
+        cityLine = driver.findElement(By.className("region-form-opener")).findElement(By.className("link__inner"));
+
+        Assert.assertEquals(cityLine.getText(), myCity.getText(),
+                "City name on the top of the page is not equal to the city name in settings");
+
+
     }
 
-    //@AfterTest()
+    @AfterTest()
     public void closeBrowser()
     {
         driver.close();
